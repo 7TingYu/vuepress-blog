6e011f49ec1051606d83b4cdb91cb44c8ad609f4
sd_liurs <liurs@gzsendi.cn>
2021-06-02 11:14:08 +0800

    refactor(blogs): 目录修改，添加内容

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,12 +1,9 @@
 const { createSideBarConfig } = require('./util')
-const VUE_PATH = '/blogs/vue-base'
-// const TIPS_PATH = '/blogs/tips'
-// const TEST_PATH = '/blogs/test'
-// const DEMO_PATH = '/blogs/demo'
+const VUE_BASE_PATH = '/blogs/vue-base'
+const VUE_ADVANCE_PATH = '/blogs/vue-advance'
 
 module.exports = {
-  [VUE_PATH]: [createSideBarConfig('vue 基础学识', VUE_PATH)],
-  // [TIPS_PATH]: [createSideBarConfig('小识书屋', TIPS_PATH)],
-  // [TEST_PATH]: [createSideBarConfig('测试', TEST_PATH)],
-  // [DEMO_PATH]: [createSideBarConfig('demo', DEMO_PATH)],
+  [VUE_BASE_PATH]: [createSideBarConfig('vue 基础学识', VUE_BASE_PATH), createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
+  [VUE_ADVANCE_PATH]: [createSideBarConfig('vue 基础学识', VUE_BASE_PATH), createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
+  // [VUE_ADVANCE_PATH]: [createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
 }
