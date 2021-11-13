60832c3b07957f5d84b24a3024c8557a87c02497
伍拾叁 <1773957200@qq.com>
2021-11-14 01:09:48 +0800

    create(c): 创建c

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,9 +1,11 @@
 const { createSideBarConfig } = require('./util')
 const VUE_BASE_PATH = '/blogs/vue-base'
 const VUE_ADVANCE_PATH = '/blogs/vue-advance'
+const C_BASE_PATH = '/blogs/c-base'
 
 module.exports = {
   [VUE_BASE_PATH]: [createSideBarConfig('vue 基础学识', VUE_BASE_PATH), createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
   [VUE_ADVANCE_PATH]: [createSideBarConfig('vue 基础学识', VUE_BASE_PATH), createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
+  [C_BASE_PATH]: [createSideBarConfig('c语言 基础学识', C_BASE_PATH)],
   // [VUE_ADVANCE_PATH]: [createSideBarConfig('vue 进阶学识', VUE_ADVANCE_PATH)],
 }
