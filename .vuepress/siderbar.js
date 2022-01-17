3d6d36c78929a79a656d7a159c7c9814b71a9235
sd_liurs <liurs@gzsendi.cn>
2022-01-17 15:56:13 +0800

    fix：更新一下

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -2,19 +2,19 @@ const { createSideBarConfig, createSideBarsConfig } = require('./util')
 
 module.exports = {
   ...createSideBarsConfig([
-    { title: 'vue 基础学识', prefixPath: '/blogs/vue-base' },
-    { title: 'vue 进阶学识', prefixPath: '/blogs/vue-advance' }
+    { title: "vue 基础学识", prefixPath: "/blogs/vue-base" },
+    { title: "vue 进阶学识", prefixPath: "/blogs/vue-advance" },
   ]),
   ...createSideBarsConfig([
-    { title: 'c语言 先导课', prefixPath: '/blogs/c-base' },
+    { title: "c语言 c 基础", prefixPath: "/blogs/c-base" },
+    { title: "c语言 数据结构", prefixPath: "/blogs/c-data-structure" },
+    { title: "c语言 c++ 基础", prefixPath: "/blogs/c++-base" },
   ]),
   ...createSideBarsConfig([
-    { title: '英语 先导课', prefixPath: '/blogs/English-base' },
+    { title: "英语 先导课", prefixPath: "/blogs/English-base" },
   ]),
   ...createSideBarsConfig([
-    { title: '数学 先导课', prefixPath: '/blogs/Math-base' },
+    { title: "数学 先导课", prefixPath: "/blogs/Math-base" },
   ]),
-  ...createSideBarsConfig([
-    { title: '小识书屋', prefixPath: '/blogs/tips' },
-  ]),
-}
+  ...createSideBarsConfig([{ title: "小识书屋", prefixPath: "/blogs/tips" }]),
+};
